d8d9eace1085bd25f0a1a26485349e18c7d52a6a
Jerry Hwang <jerryhwang@outlook.com>
2019-01-22 16:11:48 -0800

    Update

---
+++ basic_function.py
@@ -0,0 +1,14 @@
+mySentence = 'loves the color'
+
+color_list = ['red','blue','green','pink','white','black']
+
+def color_function(name):
+    lst = []
+    for i in color_list:
+        msg = "{0} {1} {2}".format(name,mySentence,i)
+        lst.append(msg)
+    return lst
+
+lst = color_function('Jesse')
+for i in lst:
+    print(i)
