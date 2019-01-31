00e976ede69829beb7c83a2c9295f4b743dddfb9
Jerry Hwang <jerryhwang@outlook.com>
2019-01-30 21:25:11 -0800

    Update

---
+++ txt_file_check.py
@@ -0,0 +1,34 @@
+# Python 3.7.2
+# Use: check a specific folder to verify whether the files end with a ".txt" file extension
+# if they do, print those qualifying file names and their corresponding modified time-stamps
+# to the console
+# 
+# Example of:
+# os module import
+# listdir() method
+# path.join() method
+# getmtime() method
+# loop
+
+import os
+
+
+def txt_file_check():
+    # specify a folder
+    fPath = 'C:\\Temp\\'
+	# get all files within a specific directory (C:\Temp\)
+    files = list(os.listdir(fPath))
+    # iterate through all files in a specified folder
+    for fName in files:
+        # check whether each file extension is txt
+        if fName.endswith(".txt"):
+            # get the concatenated file name
+        	abPath = os.path.join(fPath,fName)
+            # check the file's last update time
+        	fDate = os.path.getmtime(abPath)
+            # print the result with formatting
+        	print("The found text file is '{}' and its last update time is '{}'.".format(abPath,fDate))
+    
+
+if __name__ == "__main__":
+	txt_file_check()
