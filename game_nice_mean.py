8c2b29b6903228c494ce81dde963e32be7f67ece
Jerry Hwang <jerryhwang@outlook.com>
2019-03-03 15:18:15 -0800

    Update

--- game_nice_mean.py
+++ game_nice_mean.py
@@ -11,13 +11,7 @@ def start(nice=0,mean=0,name=""):
 
 
 def describe_game(name):
-    """
-        Check if this is a new game or not.
-        If it is new, get the user's name.
-        If it is not a new game, thank the player for
-        playing again and continue with the game.
-    """
-    # meaning, if we do not already have this user's name,
+    # if we do not already have this user's name,
     # then they are a new player and we need to get their name
     if name != "":
         print("\nThank you for playing again, {}!".format(name))
@@ -31,7 +25,7 @@ def describe_game(name):
                     print("\nIn this game, you will be greeted \nby several different people. \nYou can chooose to be nice or mean")
                     print("but at the end of the game your gate \nwill be sealed by your actions")
                     stop = False
-    return name
+    return name # return name to start()
 
 
 def nice_mean(nice,mean,name):
