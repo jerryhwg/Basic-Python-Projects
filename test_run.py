130dd2b70099449d6db24101bcb0f3bb26c2d24b
Jerry Hwang <jerryhwang@outlook.com>
2019-04-16 10:22:47 -0700

    update

--- test_run.py
+++ test_run.py
@@ -708,3 +708,31 @@ my_ecar.drive()
 
 
 # Instances as attributes
+
+class Battery():
+    def __init__(self, size=70):
+        self.size = size
+        self.charge_level = 0
+    def get_range(self):
+        if self.size == 70:
+            return 240
+        elif self.size == 85:
+            return 270
+
+# Using an instance as an attribute
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def charge(self):
+        self.battery.charge_level = 100
+        print("The vehicle is fully charged.")
+
+# Using the instance
+
+my_ecar = ElectricCar('tesla', 'model x', 2016)
+my_ecar.charge()
+print(my_ecar.battery.get_range())
+my_ecar.drive()
